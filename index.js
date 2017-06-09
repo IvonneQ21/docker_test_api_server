4387b6db3170941bfbcb0ce4cf07afb78991cbf9
joshwyatt <joshuawyatt123@gmail.com>
2017-06-09 09:43:25 -0700

    Clean up comments

--- index.js
+++ index.js
@@ -18,38 +18,3 @@ app.get('/decrement', decrementHandler);
 app.get('/reset', resetHandler);
 
 app.listen(8000, () => console.log('listening on 8000'));
-
-// function getCountHandler(req, res) {
-//   count.get()
-//     .then(count => res.json({count, containerID}))
-//     .catch(err => {
-//       res.status(500).json({ err })
-//     });
-// }
-
-// function incrementHandler(req, res) {
-//   count.increment()
-//     .then(count => res.json({count, containerID}))
-//     .catch(err => {
-//       res.status(500).json({ err })
-//     });
-// }
-
-// function decrementHandler(req, res) {
-//   count.decrement()
-//     .then(count => res.json({count, containerID}))
-//     .catch(err => {
-//       res.status(500).json({ err })
-//     });
-// }
-
-// function resetHandler(req, res) {
-//   count.reset()
-//     .then(okMessage => {
-//       let count = 0;
-//       res.json({count, containerID})
-//     })
-//     .catch(err => {
-//       res.status(500).json({ err })
-//     });
-// }
